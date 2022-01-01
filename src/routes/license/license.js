3d03bc84df226d2bf064b0c72ec38f970a471c73
JoseO1985 <ordicu85@gmail.com>
2022-01-01 20:25:28 +0000

    Testing upload csv endpoints

--- src/routes/license/license.js
+++ src/routes/license/license.js
@@ -6,17 +6,17 @@ const upload = multerUploader.single('file');
 export const licenseRouter = Router();
 
 licenseRouter.post('/upload-csv', (req, res) => {
-  try {
-    upload(req, res, async function (err) {
-      if (err || !req.file) {
-        return res.status(400).json({ message: 'Error uploading file' });
-      } else {
+  upload(req, res, async function (err) {
+    if (err || !req.file) {
+      res.status(400).json({ message: 'Error uploading file' });
+    } else {
+      try {
         const { originalname, filename, size } = req.file;
         const uploadedFile = await Upload.create({ originalname, filename, size, processed: false });
         res.send(uploadedFile);
+      } catch (err) {
+        res.status(400).json({ error: err.message });
       }
-    });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+    }
+  });
 });
---
+++ src/routes/license/license.test.js
@@ -0,0 +1,73 @@
+import supertest from 'supertest';
+import { getTestableApi } from '../../tests/utils';
+
+import { licenseRouter } from './license';
+import Upload from '../../models/Upload';
+import multerUploader from '../../middlewares/multer/multer';
+
+const app = getTestableApi('/license', licenseRouter);
+const api = supertest(app);
+
+let mockUploadedFile = [
+  {
+    originalname: 'partner01.txt',
+    mimetype: 'text/csv',
+    filename: '1640900262126-partner01.csv',
+    path: 'src\\uploads\\1640900262126-partner01.csv',
+  },
+];
+
+jest.mock('../../models/Upload');
+jest.mock('../../middlewares/multer/multer', () => {
+  const originalMulter = jest.requireActual('../../middlewares/multer/multer');
+  return {
+    ...originalMulter,
+    single: () => {
+      return (req, res, next) => {
+        req.file = mockUploadedFile;
+        next();
+      };
+    },
+  };
+});
+
+describe('/upload-csv', () => {
+  test('returns status 200 if the uploaded file is valid', async (done) => {
+    Upload.create.mockReturnValueOnce({
+      originalname: 'partner01.csv',
+      filename: '1640902140933-partner01.csv',
+      processed: false,
+      id: '61ce2dff8f33a8338474ee5d',
+    });
+    const file = Buffer.from('file content');
+    await api
+      .post('/license/upload-csv')
+      .send({ file: file })
+      //.attach('file', './src/uploads/1640899018344-partner01.csv')
+      .expect(200)
+      .expect({
+        originalname: 'partner01.csv',
+        filename: '1640902140933-partner01.csv',
+        processed: false,
+        id: '61ce2dff8f33a8338474ee5d',
+      });
+    done();
+  });
+
+  test('returns status 400 if an error is thrown inside body function', async (done) => {
+    Upload.create.mockImplementation(() => {
+      throw new Error();
+    });
+    const file = Buffer.from('file content');
+    await api.post('/license/upload-csv').send({ file: file }).expect(400);
+    done();
+  });
+
+  test('returns status 400 if uploaded file is invalid', async (done) => {
+    Upload.create.mockReturnValueOnce({});
+    mockUploadedFile = null;
+    const file = Buffer.from('file content');
+    await api.post('/license/upload-csv').send({ file: file }).expect(400).expect({ message: 'Error uploading file' });
+    done();
+  });
+});
