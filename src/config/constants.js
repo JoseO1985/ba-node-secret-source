7bbe3536b11aa3481136dcbc1e8883b79df3569f
JoseO1985 <ordicu85@gmail.com>
2022-01-01 21:02:07 +0000

    Adding constants file

---
+++ src/config/constants.js
@@ -0,0 +1,5 @@
+const UPLOAD_PATH = "/uploads/";
+const LICENSE_PDF_PATH = "/assets/licenses/";
+const PDF_TEMPLATE_PATH = "/assets/templates/license.html";
+
+export { UPLOAD_PATH, LICENSE_PDF_PATH, PDF_TEMPLATE_PATH };
