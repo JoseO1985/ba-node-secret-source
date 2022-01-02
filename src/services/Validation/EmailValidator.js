c602c17072881e77186da9f61d2f53ca0cbb8ff8
JoseO1985 <ordicu85@gmail.com>
2022-01-02 22:29:26 +0000

    Adding email validator

---
+++ src/services/Validation/EmailValidator.js
@@ -0,0 +1,13 @@
+import License from '../../models/License';
+
+export default class EmailValidator {
+  static isValid(email) {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  }
+
+  static exists(email) {
+    return License.findOne({ email: email });
+  }
+}
