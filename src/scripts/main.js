9f279b3e2b0c73cf4f3c51bc0dd5d0bed84eb9b7
JoseO1985 <ordicu85@gmail.com>
2022-01-02 22:30:43 +0000

    Updating upload processor dependencies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,12 @@ import { CronJob } from 'cron';
 import dbConnection from '../db/connection';
 import UploadProcessor from './UploadProcessor';
 import CSVReader from '../services/FileReader/CSVReader';
-import { UPLOAD_PATH } from '../config/constants';
+import PDFWriter from '../services/FileWriter/PDFWriter';
+import EmailNotifier from '../services/Notification/EmailNotifier';
+import { UPLOAD_PATH, PDF_TEMPLATE_PATH, LICENSE_PDF_PATH } from '../config/constants';
 
 export default class ScheduledTasks {
   static initAll() {
-    console.log({ __basedir });
     console.log('Starting all scripts');
     dbConnection.connect().then(() => {
       ScheduledTasks.initProcessor();
@@ -15,8 +16,10 @@ export default class ScheduledTasks {
 
   static initProcessor() {
     const csvReader = new CSVReader(UPLOAD_PATH);
+    const pdfWriter = new PDFWriter(LICENSE_PDF_PATH, PDF_TEMPLATE_PATH);
+    const emailNotifier = new EmailNotifier();
 
-    const uploadProcessor = new UploadProcessor(csvReader);
+    const uploadProcessor = new UploadProcessor(csvReader, pdfWriter, emailNotifier);
     const task = new CronJob({
       cronTime: '0 0/1 * * * *',
       onTick: () => {
