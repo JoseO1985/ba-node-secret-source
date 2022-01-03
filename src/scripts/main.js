5afb2139dabb67454d2b473bbb96aa60ef61062b
JoseO1985 <ordicu85@gmail.com>
2022-01-03 00:08:01 +0000

    Scheduling nightly scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,13 @@ import CSVReader from '../services/FileReader/CSVReader';
 import PDFWriter from '../services/FileWriter/PDFWriter';
 import EmailNotifier from '../services/Notification/EmailNotifier';
 import { UPLOAD_PATH, PDF_TEMPLATE_PATH, LICENSE_PDF_PATH } from '../config/constants';
-
+import Report from './Report';
 export default class ScheduledTasks {
   static initAll() {
     console.log('Starting all scripts');
     dbConnection.connect().then(() => {
       ScheduledTasks.initProcessor();
+      ScheduledTasks.initReport();
     });
   }
 
@@ -20,10 +21,27 @@ export default class ScheduledTasks {
     const emailNotifier = new EmailNotifier();
 
     const uploadProcessor = new UploadProcessor(csvReader, pdfWriter, emailNotifier);
+
     const task = new CronJob({
-      cronTime: '0 0/1 * * * *',
+      cronTime: '0 0 23 * * *',
       onTick: () => {
         uploadProcessor.processPendingUploads();
+      },
+      start: true,
+    });
+    task.start();
+  }
+
+  static initReport() {
+    const emailNotifier = new EmailNotifier();
+
+    const report = new Report(emailNotifier);
+
+    const task = new CronJob({
+      cronTime: '0 0 23 * * *',
+      onTick: () => {
+        const date = new Date();
+        report.sendNightlySummary(date);
       },
       start: true,
     });
