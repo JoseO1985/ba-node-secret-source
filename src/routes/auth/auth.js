3f6eb079ed06f808718d88e46d62304e2db0749b
JoseO1985 <ordicu85@gmail.com>
2022-01-11 09:22:18 +0000

    Adding role and fixing signup issues

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -1,14 +1,19 @@
 import { Router } from 'express';
-import User from "../../models/User";
-import Password from "../../services/Password/Password";
-import Token from "../../services/Token/Token";
+import User from '../../models/User';
+import Password from '../../services/Password/Password';
+import Token from '../../services/Token/Token';
+import EmailValidator from '../../services/Validation/EmailValidator';
+import joi from 'joi';
 
 export const authRouter = Router();
 
 authRouter.post('/login', async (req, res) => {
   try {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     const { email, password } = req.body;
-    if (!email || !password) throw new Error('Missing data');
+    /*if (!email || !password) throw new Error('Missing data');
+    if (!EmailValidator.isValid(email)) throw new Error('Invalid email');*/
 
     const user = await User.findOne({ email });
     if (!user) throw new Error('Invalid credentials');
@@ -16,7 +21,7 @@ authRouter.post('/login', async (req, res) => {
     const hashedPassword = Password.hashPassword(password);
     if (!Password.compare(password, hashedPassword)) throw new Error('Invalid password');
 
-    const token = Token.generateToken(user.id);
+    const token = Token.generateToken(user.id, user.role);
     res.status(200).json({ token, userId: user.id });
   } catch (err) {
     res.status(400).json({ error: err.message });
@@ -25,15 +30,23 @@ authRouter.post('/login', async (req, res) => {
 
 authRouter.post('/signup', async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) throw new Error("Missing data");
+    const { username, email, password, role } = req.body;
+    if ((!username || !email || !password, !role)) throw new Error('Missing data');
+    if (!EmailValidator.isValid(email)) throw new Error('Invalid email');
 
     const hashedPassword = Password.hashPassword(password);
-    await User.create({ username, email, password: hashedPassword });
+    const user = await User.create({ username, email, password: hashedPassword, role });
 
-    res.status(201).json();
+    res.status(201).json(user);
   } catch (e) {
     res.status(400).json({ error: e.message });
   }
-})
+});
 
+const validate = (user) => {
+  const schema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+  return schema.validate(user);
+};
